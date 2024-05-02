7ecc2f246453254f5990597551422ac3552ec0ca
Thomas Stockwell <thomas.stockwell@gmail.com>
2024-05-01 21:11:32 -0400

    Knowledge added to random furnisher bags from writ vendors.

--- FurnishingRecipeCollector/FurnishingRecipeCollector.lua
+++ FurnishingRecipeCollector/FurnishingRecipeCollector.lua
@@ -37,13 +37,13 @@ if LCK ~= nil then
   }
 end
 
-local function adjustToolTip(tooltipControl, method, itemLink)
+local function adjustToolTip(tooltipControl, itemLink)
   local fontStyle = "MEDIUM_FONT"
   local fontSizeH1 = 14
   local fontSizeH2 = 12
   local fontWeight = "soft-shadow-thin"
   local itemLinkId = GetItemLinkItemId(itemLink)
-  if FRC.logger ~= nil then FRC.logger:Info(tos(itemLink).." #"..tos(itemLinkId)) end
+  if FRC.logger ~= nil then FRC.logger:Verbose(tos(itemLink).." #"..tos(itemLinkId)) end
 
   --If present item is a folio, then add items to the tooltip
   if FRC.Data.Folios[itemLinkId] ~= nil then
@@ -62,27 +62,65 @@ local function adjustToolTip(tooltipControl, method, itemLink)
               if FRC.logger ~= nil then FRC.logger:Verbose(tos(knowledge["name"]).." "..tos(knowledge["knowledge"])) end
 
               if knowledge.knowledge == LCK.KNOWLEDGE_KNOWN or knowledge.knowledge == LCK.KNOWLEDGE_UNKNOWN then
-                if i == 1 then
-                  characterstring = string.format("|c%06X%s|r", FRC.Colors[knowledge.knowledge], knowledge["name"])
-                else
-                  characterstring = characterstring..","..string.format("|c%06X%s|r", FRC.Colors[knowledge.knowledge], knowledge["name"])
+
+                if characterstring ~= "" then
+                  characterstring = characterstring..", "
                 end
+                characterstring = characterstring..string.format("|c%06X%s|r", FRC.Colors[knowledge.knowledge], knowledge["name"])
               end
             end
             tooltipControl:AddLine("Known By: "..characterstring,string.format("$(%s)|$(KB_%s)|%s", fontStyle, fontSizeH2, fontWeight))
         end
       end
     end
+  elseif
+  --If present item in a furnisher grab bag, then add collective details for characters
+    FRC.Data.FurnisherDocuments[itemLinkId] ~= nil and LCK ~= nil then
+    ZO_Tooltip_AddDivider(tooltipControl)
+
+    local tChars = LCK.GetCharacterList()
+    local characterstring = ""
+    local charactercolor = ""
+
+    for i, tChr in pairs(tChars) do
+      --if FRC.logger ~= nil then FRC.logger:Info(tChr["name"]) end
+      tChr.known = 0
+      tChr.tracked = 0
+
+      for j,recipeId in ipairs(FRC.Data.FurnisherDocuments[itemLinkId]) do
+        local knowl = LCK.GetItemKnowledgeForCharacter(recipeId, nil,tChr["id"])
+        if knowl == LCK.KNOWLEDGE_KNOWN then
+          tChr.tracked = tChr.tracked + 1
+          tChr.known = tChr.known + 1
+        elseif knowl == LCK.KNOWLEDGE_UNKNOWN then
+          tChr.tracked = tChr.tracked + 1
+        end
+      end
+      if tChr.known == 0 then
+        charactercolor = 0x777766
+      elseif tChr.known == table.getn(FRC.Data.FurnisherDocuments[itemLinkId]) then
+        charactercolor = 0x55ff1c
+      else
+        charactercolor = 0x3399FF
+      end
+      if tChr.tracked > 0 then
+        if characterstring ~= "" then
+          characterstring = characterstring..", "
+        end
+        characterstring = characterstring..string.format("|c%06X%s|r", charactercolor, tChr["name"].."("..tChr.known.."/"..table.getn(FRC.Data.FurnisherDocuments[itemLinkId])..")")
+      end
+    end
+
+    tooltipControl:AddLine("Known By: "..characterstring,string.format("$(%s)|$(KB_%s)|%s", fontStyle, fontSizeH1, fontWeight))
   end
 
 end
 local function TooltipHook(tooltipControl, method, linkFunc)
 	local origMethod = tooltipControl[method]
-
 	tooltipControl[method] = function(self, ...)
 		origMethod(self, ...)
 
-    adjustToolTip(tooltipControl, method, linkFunc(...))
+    adjustToolTip(tooltipControl, linkFunc(...))
 	end
 end
 
@@ -92,28 +130,32 @@ local function TooltipHook_Gamepad(tooltipControl, method, linkFunc)
 	tooltipControl[method] = function(self, ...)
 		local result = origMethod(self, ...)
 
-    adjustToolTip(tooltipControl, method, linkFunc(...))
+    adjustToolTip(tooltipControl, linkFunc(...))
 
     return result
 	end
 end
-
+local ItemLinkPassthrough = function( itemLink )
+  return itemLink
+end
 local function HookTooltips()
+
+	TooltipHook(PopupTooltip, "SetLink", ItemLinkPassthrough)
+	TooltipHook(ItemTooltip, "SetLink", ItemLinkPassthrough)
 	TooltipHook(ItemTooltip, "SetBagItem", GetItemLink)
 	TooltipHook(ItemTooltip, "SetTradeItem", GetTradeItemLink)
 	TooltipHook(ItemTooltip, "SetBuybackItem", GetBuybackItemLink)
 	TooltipHook(ItemTooltip, "SetStoreItem", GetStoreItemLink)
 	TooltipHook(ItemTooltip, "SetAttachedMailItem", GetAttachedItemLink)
 	TooltipHook(ItemTooltip, "SetLootItem", GetLootItemLink)
+	TooltipHook(ItemTooltip, "SetReward", GetItemRewardItemLink)
+	TooltipHook(ItemTooltip, "SetQuestReward", GetQuestRewardItemLink)
 	TooltipHook(ItemTooltip, "SetTradingHouseItem", GetTradingHouseSearchResultItemLink)
 	TooltipHook(ItemTooltip, "SetTradingHouseListing", GetTradingHouseListingItemLink)
-	TooltipHook(ItemTooltip, "SetLink", ReturnItemLink)
 
-	TooltipHook(PopupTooltip, "SetLink", ReturnItemLink)
-
-	TooltipHook_Gamepad(GAMEPAD_TOOLTIPS:GetTooltip(GAMEPAD_LEFT_TOOLTIP), "LayoutItem", ReturnItemLink)
-	TooltipHook_Gamepad(GAMEPAD_TOOLTIPS:GetTooltip(GAMEPAD_RIGHT_TOOLTIP), "LayoutItem", ReturnItemLink)
-	TooltipHook_Gamepad(GAMEPAD_TOOLTIPS:GetTooltip(GAMEPAD_MOVABLE_TOOLTIP), "LayoutItem", ReturnItemLink)
+	TooltipHook_Gamepad(GAMEPAD_TOOLTIPS:GetTooltip(GAMEPAD_LEFT_TOOLTIP), "LayoutItem", ItemLinkPassthrough)
+	TooltipHook_Gamepad(GAMEPAD_TOOLTIPS:GetTooltip(GAMEPAD_RIGHT_TOOLTIP), "LayoutItem", ItemLinkPassthrough)
+	TooltipHook_Gamepad(GAMEPAD_TOOLTIPS:GetTooltip(GAMEPAD_MOVABLE_TOOLTIP), "LayoutItem", ItemLinkPassthrough)
 end
 
 --[[
