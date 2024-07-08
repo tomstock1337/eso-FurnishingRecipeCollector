9f3ba650e1d00df84cce27246e88f5fd9ab27515
Thomas Stockwell <thomas.stockwell@gmail.com>
2024-07-08 16:51:59 -0400

    Faustina/Rolis/Folio Shuffle
    (cherry picked from commit 719e418907fe9e81f2d4c21c1c1d7710437f9376)

--- FurnishingRecipeCollector/dataMisc.lua
+++ FurnishingRecipeCollector/dataMisc.lua
@@ -3,24 +3,25 @@ local FRC = FurnishingRecipeCollector
 FRC.Data = FRC.Data or {}
 --https://en.uesp.net/wiki/Online:Furnisher_Documents
 FRC.Data.Misc = {
-  [198062]={location="Faustina Writ Vendor"}, --Blueprint: Necrom Cart, Funerary
-  [198065]={location="Faustina Writ Vendor"}, --Design: Indoril Chandelier, Vine-Covered
-  [198061]={location="Faustina Writ Vendor"}, --Diagram: Dwarven Door, Bronze
-  [198064]={location="Faustina Writ Vendor"}, --Formula: Telvanni Lantern, Luminous Mushroom
-  [198060]={location="Faustina Writ Vendor"}, --Pattern: Dark Elf Tent, Multiroom
-  [198063]={location="Faustina Writ Vendor"}, --Praxis: Necrom Crematory, Furnace
-  [198059]={location="Faustina Writ Vendor"}, --Sketch: Daedric Mirror, Nightmarish
-  [203319]={location="Rolis Hlaalu Writ Vendor"}, --Blueprint: Pergola, Reclaimed Wood
-  [203320]={location="Rolis Hlaalu Writ Vendor"}, --Design: Apocrypha Tree, Spore
-  [203323]={location="Rolis Hlaalu Writ Vendor"}, --Diagram: Apocrypha Bookcase Platform
-  [203321]={location="Rolis Hlaalu Writ Vendor"}, --Formula: Apocrypha Light Diffuser, Stalk
-  [203322]={location="Rolis Hlaalu Writ Vendor"}, --Pattern: Apocrypha Book Press
-  [203325]={location="Rolis Hlaalu Writ Vendor"}, --Praxis: Apocrypha Wall, Eye
+  [203319]={location="Faustina Writ Vendor"}, --Blueprint: Pergola, Reclaimed Wood
+  [203320]={location="Faustina Writ Vendor"}, --Design: Apocrypha Tree, Spore
+  [203323]={location="Faustina Writ Vendor"}, --Diagram: Apocrypha Bookcase Platform
+  [203321]={location="Faustina Writ Vendor"}, --Formula: Apocrypha Light Diffuser, Stalk
+  [203322]={location="Faustina Writ Vendor"}, --Pattern: Apocrypha Book Press
+  [203325]={location="Faustina Writ Vendor"}, --Praxis: Apocrypha Wall, Eye
+  [203324]={location="Faustina Writ Vendor"}, --Sketch: Apocrypha Mirror, Intricate
+  [211033]={location="Rolis Hlaalu Writ Vendor"}, --Formula: Colovian Alembic Set, Colorful
+  [211034]={location="Rolis Hlaalu Writ Vendor"}, --Diagram: Colovian Chandelier, Grapes
+  [211035]={location="Rolis Hlaalu Writ Vendor"}, --Pattern: Colovian Tapestry, Red Diamond
+  [211036]={location="Rolis Hlaalu Writ Vendor"}, --Praxis: Colovian Glassblower's Furnace
+  [211037]={location="Rolis Hlaalu Writ Vendor"}, --Sketch: Colovian Mirror, Standing
+  [211038]={location="Rolis Hlaalu Writ Vendor"}, --Design: Dawnwood Platter, Feast
+  [211039]={location="Rolis Hlaalu Writ Vendor"}, --Blueprint: Colovian Keg, Gigantic Wine
+  --The following are always at Rolis
   [126582]={location="Rolis Hlaalu Writ Vendor"}, --Praxis: Target Centurion, Dwarf-Brass
   [126583]={location="Rolis Hlaalu Writ Vendor"}, --Praxis: Target Centurion, Robust Refabricated
   [119592]={location="Rolis Hlaalu Writ Vendor"}, --Praxis: Target Skeleton, Humanoid
   [121315]={location="Rolis Hlaalu Writ Vendor"}, --Praxis: Target Skeleton, Robust Humanoid
-  [203324]={location="Rolis Hlaalu Writ Vendor"}, --Sketch: Apocrypha Mirror, Intricate
   [119391]={location="Brewer"}, --Design: Common Candle, Set
   [139487]={location="Nalirswewen, Artaeum"}, --Praxis: Book Row, Levitating
   [139488]={location="Nalirswewen, Artaeum"}, --Praxis: Book Stack, Levitating
